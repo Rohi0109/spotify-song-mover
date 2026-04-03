59abe1544541cf3184a00e7e0537a2891270331d
rohi0109 <rohi0109@gmail.com>
2026-04-03 23:59:31 +0000

    cleanup

--- src/utils/downloading_to_navidrome.rs
+++ src/utils/downloading_to_navidrome.rs
@@ -15,7 +15,6 @@ pub async fn downloading_songs(songs: &[(String, SongStats)]) -> Result<(), Box<
         PathBuf::from("libs"),
         PathBuf::from("output")
     ).await?
-        .with_cookies_from_browser("firefox")  // Use cookies from Firefox for age-restricted videos
         .build().await?;
 
 
